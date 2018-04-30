fcd39aade310421d3813ff504331b91467dfd533
acarrill <a.jimenezcarr@alumnos.urjc.es>
2018-04-30 16:43:47 +0200

    added: xyz equations added

--- lab2xyz.m
+++ lab2xyz.m
@@ -10,9 +10,6 @@ b = lab(:,:,3);
 x = zeros(size(l));
 y = zeros(size(l));
 z = zeros(size(l));
-xyz(:,:,1) = x;
-xyz(:,:,2) = y;
-xyz(:,:,3) = z;
 
 % Define reference tristimulus values. CIE RGB E
 x0 = 0.4887180 + 0.3106803 + 0.2006017;
@@ -23,10 +20,17 @@ z0 = 0.0000000 + 0.0102048 + 0.9897952;
 P = (l.+16)/116;
 
 % Identity for assigment 
-id = P^3 > 0.008856;
+id = P.^3 > 0.008856;
 
 % lab to xyz equations
-x(id) = x0*(P + a/500)^3;
+x(id) = x0*(P + a/500).^3;
+y(id) = y0*P.^3;
+z(id) = z0*(P-b/200).^3
+
+% Return
+xyz(:,:,1) = x;
+xyz(:,:,2) = y;
+xyz(:,:,3) = z;
 
 
 
