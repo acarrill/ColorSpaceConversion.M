f2c6dec1edccc56970369c264cae88dc1625e1cc
acarrill <a.jimenezcarr@alumnos.urjc.es>
2018-05-01 21:48:07 +0200

    Added: hsi and hsv case variables assigment and corrected assigment in conditions for rgb equalities

--- hsx2rgb.m
+++ hsx2rgb.m
@@ -19,12 +19,28 @@ ht = h/(1/3*pi);
 I = c.*(1-abs(mod(ht,2)-1));
 m = x-c/2;
 
-if 0<=ht<=1, r=c; g=x; b=0; end;
-if 1<=ht<=2, r=c; g=x; b=0; end;
-if 2<=ht<=3, r=c; g=x; b=0; end;
-if 3<=ht<=4, r=c; g=x; b=0; end;
-if 4<=ht<=5, r=c; g=x; b=0; end;
-if 5<=ht<=6, r=c; g=x; b=0; end;
+% Assigment in hsv case
+if LIV=='v',
+   c = x.*s;
+   m = x-c;
+end;
+
+% Assigment in hsi case
+if LIV=='i'
+   z = (1-abs(mod(ht,2)-1));
+   c = (3*x*s)./(1+z);
+   I = c.*z;
+   m = x-c;
+end;
+
+% Conditions for rgb variables assigment
+if 0<=ht<=1, r=c; g=I; b=0; end;
+if 1<=ht<=2, r=I; g=c; b=0; end;
+if 2<=ht<=3, r=0; g=c; b=I; end;
+if 3<=ht<=4, r=0; g=I; b=c; end;
+if 4<=ht<=5, r=I; g=0; b=c; end;
+if 5<=ht<=6, r=c; g=0; b=I; end;
+
 
 % Return
 rgb(:,:,1) = r;
