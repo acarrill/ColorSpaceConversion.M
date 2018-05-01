cbf18573d9fc050c789b38e0b371c5c681ad1b60
acarrill <a.jimenezcarr@alumnos.urjc.es>
2018-05-01 21:38:08 +0200

    fixed: conditions and default variables assignment corrected

--- hsx2rgb.m
+++ hsx2rgb.m
@@ -14,10 +14,10 @@ g = zeros(size(h));
 b = zeros(size(h));
 
 % Initialize chroma, hue and m(hsl equations by default)
-c = (1-abs(2*x-1))*s;
+c = (1-abs(2*x-1)).*s;
 ht = h/(1/3*pi);
-I = c*(1-abs(mod(ht,2)-1));
-m = l-c/2;
+I = c.*(1-abs(mod(ht,2)-1));
+m = x-c/2;
 
 if 0<=ht<=1, r=c; g=x; b=0; end;
 if 1<=ht<=2, r=c; g=x; b=0; end;
@@ -25,4 +25,9 @@ if 2<=ht<=3, r=c; g=x; b=0; end;
 if 3<=ht<=4, r=c; g=x; b=0; end;
 if 4<=ht<=5, r=c; g=x; b=0; end;
 if 5<=ht<=6, r=c; g=x; b=0; end;
+
+% Return
+rgb(:,:,1) = r;
+rgb(:,:,2) = g;
+rgb(:,:,3) = b;
 
